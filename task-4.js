e452913792a9ff18a946176aa137cbab698d4897
Tatyana <tannyhappiness@gmail.com>
2020-07-30 19:49:32 +0300

    homework-01

--- task-4.js
+++ task-4.js
@@ -1,15 +1,15 @@
-"use strict";
+// "use strict";
 
-const credits = 23580;
-const pricePerDroid = 3000;
+// const credits = 23580;
+// const pricePerDroid = 3000;
 
-const quantity = prompt("Какое количество дроидов вы хотите купить?");
-const totalPrice = pricePerDroid * quantity;
+// const quantity = prompt("Какое количество дроидов вы хотите купить?");
+// const totalPrice = pricePerDroid * quantity;
 
-if (quantity == null) {
-    alert ("Отменено пользователем!")
-} else if (totalPrice > credits) {
-    alert("Недостаточно средств на счету!")
-} else if (totalPrice < credits) {
-    alert(`Вы купили ${quantity} дроидов, на счету осталось ${credits - totalPrice} кредитов.`)
-}
+// if (quantity == null) {
+//     alert ("Отменено пользователем!")
+// } else if (totalPrice > credits) {
+//     alert("Недостаточно средств на счету!")
+// } else if (totalPrice < credits) {
+//     alert(`Вы купили ${quantity} дроидов, на счету осталось ${credits - totalPrice} кредитов.`)
+// }
